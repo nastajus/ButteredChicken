8fe21c10e054853e75bd0cfe678bd5151d721284
Ian Nastajus <ian.nastajus@gmail.com>
2016-12-31 23:34:30 -0500

    collision of chicken and chef complete

--- Assets/ChickenMovement.cs
+++ Assets/ChickenMovement.cs
@@ -5,10 +5,14 @@ public class ChickenMovement : MonoBehaviour {
 
     Bounds chickenBounds;
     float direction = .2f;
+    Transform chefTransform;
+    SpriteRenderer sr;
 
 	// Use this for initialization
 	void Start () {
         chickenBounds = IanUtils.CameraOrthographicBounds(Camera.main);
+        chefTransform = GameObject.Find("chef").transform;
+        sr = GetComponent<SpriteRenderer>();
     }
 	
 	// Update is called once per frame
@@ -28,5 +32,24 @@ public class ChickenMovement : MonoBehaviour {
             print("flipped 2");
         }
 
+        if (DetectChickenOnChef(transform, chefTransform))
+        {
+            sr.color = Color.white;
+        }
+        else
+        {
+            sr.color = Color.red;
+        }
+
 	}
+
+    public bool DetectChickenOnChef(Transform chefTransform, Transform chickenTransform)
+    {
+        if (chefTransform.position.x <= chickenTransform.position.x - 1 || chefTransform.position.x >= chickenTransform.position.x + 1)
+        {
+            return true;
+        }
+        return false;
+    }
+
 }
